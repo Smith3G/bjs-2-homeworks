e4e253c73ab938b636695803dd2fafa9a7beff8a
Smith3G <mypom87@gmail.com>
2022-05-08 16:50:51 +0300

    solved task1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,22 @@
 'use strict'
 function solveEquation(a, b, c) {
   let arr;
-  arr = b ** 2 - 4 * a * c// код для задачи №1 писать здесь
-  return arr; // array
+  let root1;
+  let root2;
+  let d;
+  d = b ** 2 - 4 * a * c;
+   if (d < 0) {
+    return arr = [];
+  }
+   else if (d > 0) {
+    root1 = (-b + Math.sqrt(d)) / 2*a;
+    root2 = (-b - Math.sqrt(d)) / 2*a;
+    return arr = [root1, root2]
+  }
+   else if (d === 0) {
+    root1 = root2 = -b / (2*a);
+    return arr = [root1, root2];
+}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
