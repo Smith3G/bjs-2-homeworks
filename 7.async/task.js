e0af57ea982d16851c26148b92e4e0d6895c63f2
Smith3G <mypom87@gmail.com>
2022-06-07 18:42:15 +0300

    without 1 test

---
+++ 7.async/task.js
@@ -0,0 +1,99 @@
+//Задача №1. Будильник-колыбельная
+
+
+class AlarmClock {
+        constructor() {   
+                this.alarmCollection = [];
+                this.timerId = null;
+        };
+
+        // Функция добавления нового будильника в массив
+        addClock(time, callback, id) {
+                if (!id) {throw new Error ('error text');
+        } else if (this.alarmCollection.some(function(value) {
+                        value.id === id; // сравнивает id каждого элемента массива с переданным id(почему то не сравнивает)
+                })) {
+                console.error(`Будильник с данным ${id} уже зарегистрирован!`);
+                 return;
+                 };
+
+                this.alarmCollection.push({id, time, callback});
+        
+        };
+        
+//удаляет определённый звонок по id
+removeClock(id) {
+        let arrLength1 = this.alarmCollection.length; //Длина исходного массива
+        this.alarmCollection = this.alarmCollection.filter(function(item) {
+return item.id !== id
+}); // возвращаем массив, без элемента с заданым id
+        let arrLength2 = this.alarmCollection.length; // Длинна массива после удаления элемента
+        return arrLength1 === arrLength2; // Проверка успешности удаления элемента из массива
+
+};
+
+getCurrentFormattedTime(time) {
+        let currentDate;
+        if(time) {
+     currentDate = time;
+        } else {
+     currentDate = new Date();
+        }
+        let minutes = currentDate.getMinutes() < 10 ? ('0' +`${currentDate.getMinutes()}`) : `${currentDate.getMinutes()}`;
+        let hours = currentDate.getHours() < 10 ? ('0' +`${currentDate.getHours()}`) : `${currentDate.getHours()}`;
+        return `${hours}:${minutes}`;
+}
+
+start() {
+        let check = checkCklock.bind(this);
+        function checkCklock(alarm) {
+                if (alarm.time === this.getCurrentFormattedTime()) alarm.callback();
+        }       
+                 if (!this.timerId) {
+                                this.timerID = setInterval(function() {
+                                this.alarmCollection.forEach(item =>check(item))}, 1000)
+                                
+                                   }
+         }
+// Останавливает выполнение всех звонков
+stop() {
+        if (this.timerID) {
+                clearInterval(this.timerID); //проверка существования текущего таймера
+                this.timerID = null; // удаление значение идентификтора текущего таймера
+        }
+}
+
+printAlarms() {
+       this.alarmCollection.forEach(item => console.log(`id = ${item.id} time = ${item.time}`));
+}
+
+
+clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+};
+       
+
+};
+
+
+
+function testCase() {
+	let clock = new AlarmClock();
+	clock.addClock(clock.getCurrentFormattedTime(), () => console.log('Пора вставать!'), 55);
+	let currentDate = new Date();
+	currentDate.setMinutes(currentDate.getMinutes() + 1);
+	clock.addClock(clock.getCurrentFormattedTime(currentDate), () => { console.log('Давно Пора встать!'); clock.removeClock(55) }, 55);
+	currentDate = new Date();
+	currentDate.setMinutes(currentDate.getMinutes() + 2);
+	clock.addClock(clock.getCurrentFormattedTime(currentDate),
+		() => { console.log('Давно Давно Пора уже встать!'); clock.stop(); clock.clearAlarms() }, 2785);
+	clock.printAlarms();
+	clock.start();
+};
+
+testCase();
+
+
+
+
